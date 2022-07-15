7c6fcabbde40af395312206a1db34a1e1600ad9a
Pavel Chernook <Maklauren97@gmail.com>
2022-07-15 17:02:23 +0300

    Implemented the addition of new tasks

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-
-    let itemArray = ["Find Mike", "Buy eggs", "Feed the cat"]
+    
+    var itemArray = ["Find Mike", "Buy eggs", "Feed the cat"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,6 +41,28 @@ class TodoListViewController: UITableViewController {
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK: - Add new items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { action in
+            self.itemArray.append(textField.text ?? "")
+            
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
     }
 }
 
